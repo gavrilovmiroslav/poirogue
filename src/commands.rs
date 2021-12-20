c031c8c0f914f9b125983ad442c9a16b4489e7d6
Miroslav <gavrilov.miroslav@gmail.com>
2021-12-20 19:28:31 +0100

    Adding serialization to random enums

--- src/commands.rs
+++ src/commands.rs
@@ -1,16 +1,18 @@
 
+#[derive(PartialEq, Serialize, Deserialize)]
 pub enum FlowCommand {
     CycleViews,
     GenerateLevel,
     Exit
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Serialize, Deserialize)]
 pub enum GameFlow {
     Player,
     World,
 }
 
+#[derive(PartialEq, Serialize, Deserialize)]
 pub enum GameCommand {
     Flow(FlowCommand),
 }
